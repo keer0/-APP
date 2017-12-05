085390e5be2a86c8d0aa66a23f1cb3296a8b74a9
小丽 <1017326027@qq.com>
2017-12-05 23:32:32 +0800

    底部选项卡加了链接页面，城市位置也添加了子页面

---
+++ 融钱APP/js/index.js
@@ -0,0 +1,36 @@
+document.getElementById('address').addEventListener('tap', function() {
+	mui.openWindow({
+		url: 'address.html',
+		id: 'address'
+	});
+});
+document.getElementById('home').addEventListener('tap', function() {
+	mui.openWindow({
+		url: 'index.html',
+		id: 'home'
+	});
+});
+document.getElementById('item1').addEventListener('tap', function() {
+	mui.openWindow({
+		url: 'item1.html',
+		id: 'item1'
+	});
+});
+document.getElementById('item2').addEventListener('tap', function() {
+	mui.openWindow({
+		url: 'item2.html',
+		id: 'item2'
+	});
+});
+document.getElementById('item3').addEventListener('tap', function() {
+	mui.openWindow({
+		url: 'item3.html',
+		id: 'item3'
+	});
+});
+document.getElementById('item4').addEventListener('tap', function() {
+	mui.openWindow({
+		url: 'item4.html',
+		id: 'item4'
+	});
+});
